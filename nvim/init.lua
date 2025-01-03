a5e7d332439f143d569c124c240cb26dad724f78
solus667 <rmorenox@gmail.com>
2025-01-03 15:03:04 -0800

    updoot

--- nvim/init.lua
+++ nvim/init.lua
@@ -1,3 +1,10 @@
+require("config.lazy")
+require('lualine').setup()
+require('mini.cursorword').setup()
+require('mini.completion').setup()
+require('mini.icons').setup()
+require('neo-tree').setup()
+
 vim.opt.number = true
 vim.opt.relativenumber = true
 vim.opt.autoindent = true
@@ -6,17 +13,25 @@ vim.opt.ruler = true
 vim.opt.laststatus = 2
 vim.opt.tabstop = 4
 vim.opt.linebreak = true
-vim.opt.vb = true
+vim.opt.visualbell = true
 vim.opt.title = true
 vim.opt.smarttab = true
 vim.opt.cursorline = true
 vim.opt.jumpoptions = "stack,view"
 vim.opt.mouse = a
 vim.opt.mouse = nicr
-vim.cmd [[colorscheme default]]
+vim.cmd [[colorscheme tokyodark]]
 vim.opt.colorcolumn = "90"
 vim.opt.cursorcolumn = true 
 vim.opt.ignorecase = true
 
-vim.cmd [[packloadall]]
-vim.cmd [[syntax on]]
+vim.api.nvim_create_augroup("neotree", {})
+vim.api.nvim_create_autocmd("UiEnter", {
+  desc = "Open Neotree automatically",
+  group = "neotree",
+  callback = function()
+    if vim.fn.argc() == 0 then
+      vim.cmd "Neotree toggle"
+    end
+  end,
+})
